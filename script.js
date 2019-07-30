9d94c07954bc3a789172c27d10118a99fecf61b3
Bikram Adhikari <42631780+BiksAdhikari@users.noreply.github.com>
2019-07-30 06:37:35 +0545

    Create script.js
    added script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+function validateEmptyValues(){
+	var txtEmail = document.getElementById("txtEmail").value;
+	var txtUsername = document.getElementById("txtUsername").value;
+	var txtPassword = document.getElementById("txtPassword").value;
+	var appendError="";
+	if (txtEmail == "") {
+		appendError = "Email";
+	}
+	if(txtUsername == ""){
+		if (appendError != "") {
+			appendError = appendError +" and";
+		}
+		appendError = appendError+" Username";
+	}
+
+	if(txtPassword == ""){
+		if (appendError != "") {
+			appendError = appendError +" and";
+		}
+		appendError = appendError+" password";
+	}
+	if (appendError != "") {
+		appendError = appendError +" cannot be empty";
+		alert(appendError);
+		return false;
+	}
+		else{
+
+			return true;
+		}
+	}
+
+
